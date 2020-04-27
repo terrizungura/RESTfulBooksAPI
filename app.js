e67b2d5f7db783447477d350a7a178e877b47cf1
terrizungura <terrizun@gmail.com>
2020-04-27 04:27:13 +0200

    implemented integration testing with supertest. Used supertest to do end to end testing

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI',  {useNewUrlParser: true, useUnifiedTopology: true});
+
+if(process.env.ENV == 'Test'){
+  console.log('This is a test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test',  {useNewUrlParser: true, useUnifiedTopology: true});
+} else{
+  console.log('This is for real');
+  const db = mongoose.connect('mongodb://localhost/bookAPI-prod',  {useNewUrlParser: true, useUnifiedTopology: true});
+}
+
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
@@ -17,6 +25,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API again!');
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Running on port  + ${port}`);
 });
+
+module.exports = app;
